b16df530266d5b63f070907162aea4b20105da8b
Qian Livingstone <Qian Livingstone>
2016-11-17 23:58:45 +0800

    half

---
+++ week5/test.py
@@ -0,0 +1,118 @@
+# encoding: utf-8
+import graphlab
+import numpy as np # note this allows us to refer to numpy as np instead
+sales = graphlab.SFrame('kc_house_data.gl/')
+# In the dataset, 'floors' was defined with type string,
+# so we'll convert them to int, before using it below
+sales['floors'] = sales['floors'].astype(int)
+
+def get_numpy_data(data_sframe, features, output):
+    data_sframe['constant'] = 1 # this is how you add a constant column to an SFrame
+    # add the column 'constant' to the front of the features list so that we can extract it along with the others:
+    features = ['constant'] + features # this is how you combine two lists
+    # select the columns of data_SFrame given by the features list into the SFrame features_sframe (now including constant):
+    features_sframe = data_sframe[features]
+    # the following line will convert the features_SFrame into a numpy matrix:
+    feature_matrix = features_sframe.to_numpy()
+    # assign the column of data_sframe associated with the output to the SArray output_sarray
+    output_sarray = data_sframe[output]
+    # the following will convert the SArray into a numpy array by first converting it to a list
+    output_array = output_sarray.to_numpy()
+    return feature_matrix, output_array
+
+def predict_output(feature_matrix, weights):
+    # assume feature_matrix is a numpy matrix containing the features as columns and weights is a corresponding numpy array
+    # create the predictions vector by using np.dot()
+    predictions = np.dot(feature_matrix, weights)
+    return predictions
+
+X = np.array([[3.,5.,8.],[4.,12.,15.]])
+print X
+
+norms = np.linalg.norm(X, axis=0) # gives [norm(X[:,0]), norm(X[:,1]), norm(X[:,2])]
+print norms
+
+print X / norms
+# gives [X[:,0]/norm(X[:,0]), X[:,1]/norm(X[:,1]), X[:,2]/norm(X[:,2])]
+def normalize_features(features_matrix):
+    norms = np.linalg.norm(features_matrix, axis=0)
+    normalized_features = features_matrix / norms
+    return normalized_features, norms
+
+features, norms = normalize_features(np.array([[3.,6.,9.],[4.,8.,12.]]))
+print features
+# should print
+# [[ 0.6  0.6  0.6]
+#  [ 0.8  0.8  0.8]]
+print norms
+# should print
+# [5.  10.  15.]
+
+simple_features = ['sqft_living', 'bedrooms']
+my_output = 'price'
+(simple_feature_matrix, output) = get_numpy_data(sales, simple_features, my_output)
+
+simple_feature_matrix, norms = normalize_features(simple_feature_matrix)
+weights = np.array([1., 4., 1.])
+prediction = predict_output(simple_feature_matrix, weights)
+# ro[i] = SUM[ [feature_i]*(output - prediction + w[i]*[feature_i]) ].
+ro = np.zeros(weights.shape)
+for i in range(len(weights)):
+    ro[i] = np.dot(simple_feature_matrix[:,i], (output - prediction + weights[i] * simple_feature_matrix[:,i]))
+
+
+def lasso_coordinate_descent_step(i, feature_matrix, output, weights, l1_penalty):
+    # compute prediction
+    prediction = predict_output(feature_matrix, weights)
+    # compute ro[i] = SUM[ [feature_i]*(output - prediction + weight[i]*[feature_i]) ]
+    ro_i = np.dot(feature_matrix[:, i], (output - prediction + weights[i] * feature_matrix[:, i]))
+
+    #     ```
+    #        ┌ (ro[i] + lambda/2)     if ro[i] < -lambda/2
+    #     w[i] = ├ 0                      if -lambda/2 <= ro[i] <= lambda/2
+    #        └ (ro[i] - lambda/2)     if ro[i] > lambda/2
+    #     ```
+    if i == 0:  # intercept -- do not regularize
+        new_weight_i = ro_i
+    elif ro_i < -l1_penalty / 2.:
+        new_weight_i = ro_i + l1_penalty / 2
+    elif ro_i > l1_penalty / 2.:
+        new_weight_i = ro_i - l1_penalty / 2
+    else:
+        new_weight_i = 0.
+
+    return new_weight_i
+
+# should print 0.425558846691
+import math
+print lasso_coordinate_descent_step(1, np.array([[3./math.sqrt(13),1./math.sqrt(10)],[2./math.sqrt(13),3./math.sqrt(10)]]),
+                                   np.array([1., 1.]), np.array([1., 4.]), 0.1)
+
+
+def lasso_cyclical_coordinate_descent(feature_matrix, output, initial_weights, l1_penalty, tolerance):
+    # 定义一个change 用来判断停止
+    max_change = 1e10
+    # 当max_change > tolerance时 Loop
+    while max_change > tolerance:
+        weights = initial_weights
+        old_weights = np.zeros_like(weights)
+        # 利用for循环来更新weight，
+        for i in range(len(weights)):
+            old_weights[i] = weights[i]
+            weights[i] = lasso_coordinate_descent_step(i, feature_matrix, output, weights, l1_penalty)
+        max_change = abs((weights - old_weights).max())
+    return weights
+
+simple_features = ['sqft_living', 'bedrooms']
+my_output = 'price'
+initial_weights = np.zeros(3)
+l1_penalty = 1e7
+tolerance = 1.0
+
+(simple_feature_matrix, output) = get_numpy_data(sales, simple_features, my_output)
+(normalized_simple_feature_matrix, simple_norms) = normalize_features(simple_feature_matrix) # normalize features
+
+weights = lasso_cyclical_coordinate_descent(normalized_simple_feature_matrix, output,
+                                            initial_weights, l1_penalty, tolerance)
+
+weights
